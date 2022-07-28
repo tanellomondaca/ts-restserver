6eb17f7e85de3f0da424a6514fbee826f4a5d7c7
Tanelo <tanello.mondaca@gmail.com>
2022-07-28 09:35:04 -0400

    250. obtener usuarios

--- controller/usuario.ts
+++ controller/usuario.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from "express";
+import Usuario from "../models/usuario";
 
-export const getUsuarios = (req: Request, res: Response) => {
+export const getUsuarios = async (req: Request, res: Response) => {
+
+    const usuarios = await Usuario.findAll();
+    
     res.json({
-        msg: 'getUsuarios'
+        usuarios
     })
 }
 
-export const getUsuario = (req: Request, res: Response) => {
+export const getUsuario = async (req: Request, res: Response) => {
     const { id } = req.params;
+    
+    const usuario = await Usuario.findByPk(id);
+
+    if( !usuario ){
+        res.status(404).json({
+            msg: `No existe usuario con id ${id}`
+        })
+    }
+
 
     res.json({
         msg: 'getUsuario',
-        id
+        usuario
     })
 }
 
--- dist/controller/usuario.js
+++ dist/controller/usuario.js
@@ -1,19 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
-const getUsuarios = (req, res) => {
+const usuario_1 = __importDefault(require("../models/usuario"));
+const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarios = yield usuario_1.default.findAll();
     res.json({
-        msg: 'getUsuarios'
+        usuarios
     });
-};
+});
 exports.getUsuarios = getUsuarios;
-const getUsuario = (req, res) => {
+const getUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
+    const usuario = yield usuario_1.default.findByPk(id);
+    if (!usuario) {
+        res.status(404).json({
+            msg: `No existe usuario con id ${id}`
+        });
+    }
     res.json({
         msg: 'getUsuario',
-        id
+        usuario
     });
-};
+});
 exports.getUsuario = getUsuario;
 const postUsuario = (req, res) => {
     const { body } = req;
